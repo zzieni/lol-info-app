2b1d0fdb86abaf098aa605e0dd489d3da0e56bbc
zzi_eun <je98710@gmail.com>
2024-12-17 20:46:14 +0900

    feat: 홈페이지 백그라운드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
+    <div className='min-h-screen  flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold text-blue-600 mb-8'>
         League of Legends
       </h1>
